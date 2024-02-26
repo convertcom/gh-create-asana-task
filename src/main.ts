05228f1364268828cf85eddaffde8ec4f7e37d4f
Claudiu Rogoveanu <claudiu@convert.com>
2024-02-26 11:24:44 +0200

    new asana api 7

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const run = async (): Promise<void> => {
 
     const workspaceId = core.getInput("asana-workspace-id", { required: true });
     const projectId = core.getInput("asana-project-id", { required: true });
-    //const sectionId = core.getInput("asana-section-id", { required: true });
+    const sectionId = core.getInput("asana-section-id", { required: true });
     const taskName = core.getInput("asana-task-name", { required: true });
     const taskDescription = core.getInput("asana-task-description");
     const dueDate = core.getInput("asana-due-date");
@@ -27,17 +27,20 @@ const run = async (): Promise<void> => {
         approval_status: "pending",
         assignee_status: "upcoming",
         completed: false,
-        notes: taskDescription,
+        html_notes: taskDescription,
         is_rendered_as_separator: false,
         liked: true,
         projects: [projectId],
         due_on: dueDate,
         workspace: workspaceId,
+        assignee_section: sectionId,
         //tags: tags ? JSON.parse(tags) : "",
       },
     };
     const opts = {};
-    await tasksApiInstance.createTask(body, opts);
+    const result = await tasksApiInstance.createTask(body, opts);
+
+    return result["data"]["permalink_url"];
   } catch (error) {
     console.error(error);
     if (error instanceof Error) core.setFailed(error.message);
