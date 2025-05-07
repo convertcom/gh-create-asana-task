c643edf1b6c19430a2e1273a51b0c737fd70e8fe
Claudiu Rogoveanu <claudiu@convert.com>
2025-05-07 10:09:15 +0300

    building latest version and updating settings

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ const run = async (): Promise<void> => {
         //tags: tags ? JSON.parse(tags) : "",
       },
     };
-    console.log('payload data to be posted to Asana API', body.data);
+    console.log("payload data to be posted to Asana API", body.data);
     const opts = {};
     const result = await tasksApiInstance.createTask(body, opts);
 
